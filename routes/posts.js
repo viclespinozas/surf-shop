87558300b2cb7305ce11751011c765e1cc6c7595
Victor <victorlespinozas@gmail.com>
2020-04-02 13:07:39 +0200

    Post resource edit-destroy cloudinary

--- routes/posts.js
+++ routes/posts.js
@@ -29,7 +29,7 @@ router.get('/:id', asynErrorHandler(postShow));
 router.get('/:id/edit', asynErrorHandler(postEdit));
 
 /* PUT posts update /posts/:id/ */
-router.put('/:id', asynErrorHandler(postUpdate));
+router.put('/:id', upload.array('images', 4), asynErrorHandler(postUpdate));
 
 /* DELETE posts destroy /posts/:id/ */
 router.delete('/:id', asynErrorHandler(postDestroy));
